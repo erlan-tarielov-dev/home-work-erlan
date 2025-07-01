537d3856afbc515e6b153e7c31e3b265656831a8
erlan-tarielov-dev <erlan.tarielov.dev@gmail.com>
2025-07-01 13:27:54 +0600

    01/07/2025 work add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AddComponent from "./AddComponent";
+import NewComp from "./NewComp";
 
 function App() {
   return (
     <div className="App">
      <AddComponent test='Прверка связи!'/>
+     <NewComp/>
     </div>
   );
 }
---
+++ src/NewComp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewComp = () => {
+  return (
+    <div>
+      '01/07/2025 work'
+    </div>
+  );
+};
+
+export default NewComp;
